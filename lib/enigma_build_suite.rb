88a5867054781f6c65260310576f684519f6bba9
Benjamin Ross <benjaminross1001@gmail.com>
2017-03-25 13:32:26 -0600

    Cleans Up First  Methods

--- lib/enigma_build_suite.rb
+++ lib/enigma_build_suite.rb
@@ -3,39 +3,50 @@ require 'pry'
 require 'date'
 
 class EnigmaSuite
+  attr_reader :encryption_key, :key_array, :rotation_key, :date_key
+
   def initialize
     @encryption_key = generate_encryption_key
     @date_key = generate_date_key
     @key_array = @encryption_key.zip(@date_key)
     @rotation_key = @key_array.map { |x| x.reduce(:+)}
-    puts "Encryption Key: #{@encryption_key}"
-    puts "Date Key: #{@date_key}"
-    puts "Rotation Key: #{@rotation_key}"
     @alphabet = ('a'..'z').to_a
+    #binding.pry
   end
 
   def generate_encryption_key
-    random_encryption = rand(99999)
-    random_encryption_leading_zeroes = "%05d" % random_encryption
-    random_encryption_leading_zeroes
+    random_encryption = conv_num_to_five_dig(gen_random_five_digit_number)
     encryption_key_array = []
-    4.times do |i|
-      encryption_key_array << random_encryption_leading_zeroes.slice(i,2).to_i
-    end
+    4.times { |i| encryption_key_array << random_encryption.slice(i,2).to_i }
     encryption_key_array
-  end
-
-  def break_down_encryption_key
-
+    #binding.pry
   end
 
   def generate_date_key(days = 0)
     date = (Date.today - days).strftime("%d%m%y")
     date_squared = date.to_i**2
-    last_four_digits = date_squared.to_s.chars.slice(-4, 4)
-    last_four_digits.map(&:to_i)
+    isolate_last_four_digits(date_squared)
+  end
+
+  def isolate_last_four_digits(number)
+    number_to_array_of_characters = number.to_s.chars
+    number_to_array_of_characters.slice(-4, 4).map(&:to_i)
+  end
+
+  def gen_random_five_digit_number
+    rand(99999)
+  end
+
+  def conv_num_to_five_dig(number)
+    "%05d" % number
   end
 end
 
 
 crack = EnigmaSuite.new
+
+#binding.pry
+
+
+
+#Rotation alph.rotate(rotation_key)[index]
--- test/enigmasuite.rb
+++ test/enigmasuite.rb
@@ -4,11 +4,39 @@ require_relative '../lib/enigma_build_suite.rb'
 require 'pry'
 
 class EnigmaSuiteTest < MiniTest::Test
-
   def test_enigmasuite_creates_new_instance
     code = EnigmaSuite.new
     assert_instance_of EnigmaSuite, code
   end
 
+  def test_encryption_key_generates_four_digit_values
+    code = EnigmaSuite.new
+    assert_equal 4, code.encryption_key.length
+  end
+
+  def test_encryption_key_generates_integer_values
+    code = EnigmaSuite.new
+    assert code.encryption_key.all? { |x| x.class == Fixnum }
+  end
+
+  def test_date_key_generates_four_digit_values
+    code = EnigmaSuite.new
+    assert_equal 4, code.date_key.length
+  end
+
+  def test_date_key_generates_integer_values
+    code = EnigmaSuite.new
+    assert code.date_key.all? { |x| x.class == Fixnum }
+  end
+
+  def test_rotation_key_generates_four_digit_values
+    code = EnigmaSuite.new
+    assert_equal 4, code.rotation_key.length
+  end
+
+  def test_rotation_key_generates_integer_values
+    code = EnigmaSuite.new
+    assert code.rotation_key.all? { |x| x.class == Fixnum }
+  end
 
 end
